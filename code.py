a4ad3392f0b2ac4499cf1d5d37063b897c0ea512
BuildTools <altamiranolucas2013@gmail.com>
2025-09-21 17:56:14 +0200

    please git do not brake

--- code.py
+++ code.py
@@ -3,20 +3,21 @@ import sprog
 class TestGame(sprog.Sprog):
     def init(self):
         self.x, self.y = 0, 0
-    
+        self.addText(x=1, y=1, text="hello world!")
     def update(self):
         self.x += 1
         self.y += 1
-        
+
     def draw(self):
         d = self.display
-        
+
         d.cls(2)
         d.cls(3)
         d.cls(4)
         d.cls(5)
         d.cls(6)
         d.pset(self.x, self.y, 14)
-        
+
 game = TestGame()
 game.run()
+ # type: ignore
--- sprog.py
+++ sprog.py
@@ -19,11 +19,11 @@ from adafruit_st7735r import ST7735R
 
 def create_cheerful24_palette():
     palette = displayio.Palette(24)
-    
+
     # Cheerful-24 colors (RGB values)
     colors = [
         (15, 15, 18),      # 0 - dark black
-        (80, 83, 89),      # 1 - dark gray  
+        (80, 83, 89),      # 1 - dark gray
         (182, 191, 188),   # 2 - light gray
         (242, 251, 255),   # 3 - white
         (94, 231, 255),    # 4 - cyan
@@ -47,11 +47,11 @@ def create_cheerful24_palette():
         (235, 117, 190),   # 22 - pink
         (119, 56, 140),    # 23 - purple
     ]
-    
+
     # convert RGB to 24-bit color values
     for i, (r, g, b) in enumerate(colors):
         palette[i] = (r << 16) | (g << 8) | b
-    
+
     return palette
 
 def SprigScreen():
@@ -77,62 +77,73 @@ class SprogDisplay:
         palette = create_cheerful24_palette()
         self.bitmap = displayio.Bitmap(160, 128, 24)  # can use all 24 colors
         self.sprite = displayio.TileGrid(self.bitmap, pixel_shader=palette)
-        
+
         splash = displayio.Group()
         splash.append(self.sprite)
         screen.root_group = splash
         self.screen = screen
-        
+
     def cls(self, color=0):
         """clear screen"""
         self.bitmap.fill(color & 15)
-    
+
     def pset(self, x, y, color):
         """set pixel"""
         if 0 <= x < 160 and 0 <= y < 128:
             self.bitmap[x, y] = color & 15
-            
-    
-            
+
+
+
 class Sprog:
     def __init__(self):
         self.display = SprogDisplay(SprigScreen())
-        
+
         self.running = True
-        
+
         self.frame_count = 0
-        
-        
-        
+
+
+
     def init(self):
         """Called once at startup - override this"""
         pass
-    
+
     def update(self):
         """Called every frame before draw - override this"""
         pass
-    
+
     def draw(self):
         """Called every frame after update - override this"""
         pass
-    
+
+    def addText(self, x, y, text):
+        """Called when adding text"""
+        label = label.Label(terminalio.FONT, text=text, color=[15, 15, 18])
+
+        label.x = x
+        label.y = y
+        self.splash.append(label)
+    def clearText(self):
+        """Called when deleting text"""
+        label.Label.remove(layer=self.splash)
     def run(self):
         """Start the game loop"""
         self.init()
         frame_time = 1.0 / 30
 
-        
-        
+
+
         while self.running:
             frame_start = time.monotonic()
-            
+
             # call user methods
             self.update()
             self.draw()
+            self.clearText()
             self.display.screen.refresh()
-            
+
             self.frame_count += 1
-            
+
             # maintain framerate
             elapsed = time.monotonic() - frame_start
             if elapsed < frame_time:
