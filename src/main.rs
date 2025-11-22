8ceabdb5121cbc32b799d46ccef646980358e5ef
Lucas11 <altamiranolucas2013@gmail.com>
2025-11-22 21:44:18 +0100

    clean more code!

--- src/main.rs
+++ src/main.rs
@@ -5,57 +5,49 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
 use cyw43_pio::{DEFAULT_CLOCK_DIVIDER, PioSpi};
 use defmt::*;
+use display_interface_spi::SPIInterface;
+use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
-use embassy_rp::gpio::{Level, Output, Input, Pull};
+use embassy_rp::gpio::{Input, Level, Output, Pull};
 use embassy_rp::peripherals::{DMA_CH0, PIO0};
 use embassy_rp::pio::{InterruptHandler, Pio};
-use mipidsi::models::ST7735s;
-use static_cell::StaticCell;
-use {defmt_rtt as _, panic_probe as _};
-use embassy_rp::spi::{Spi, Config as SpiConfig};
+use embassy_rp::spi::{Config as SpiConfig, Spi};
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
-use display_interface_spi::SPIInterface;
-use mipidsi::Builder;
 use embedded_graphics::{
-    mono_font::{
-        ascii::{FONT_10X20, FONT_5X8, FONT_6X12, FONT_9X15},
-        MonoTextStyle, MonoTextStyleBuilder,
-    },
+    mono_font::{MonoTextStyle, MonoTextStyleBuilder, ascii::FONT_5X8},
     pixelcolor::BinaryColor,
     pixelcolor::Rgb565,
     prelude::*,
     text::Text,
 };
-use core::cell::RefCell;
-use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
+use mipidsi::Builder;
+use mipidsi::models::ST7735s;
+use static_cell::StaticCell;
+use {defmt_rtt as _, panic_probe as _};
 
 use embassy_sync::blocking_mutex::Mutex;
-
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
 });
 
 #[embassy_executor::task]
-async fn cyw43_task(runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>) -> ! {
+async fn cyw43_task(
+    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
+) -> ! {
     runner.run().await
 }
-
-
 
 async fn text(text: &str, x: i32, y: i32) -> Text<'_, MonoTextStyle<'_, BinaryColor>> {
     let style = MonoTextStyleBuilder::new()
         .font(&FONT_5X8)
         .text_color(BinaryColor::Off)
         .build();
-    Text::new(
-        text,
-        Point::new(x, y),
-        style        
-    )
+    Text::new(text, Point::new(x, y), style)
 }
 
 #[embassy_executor::main]
@@ -98,10 +90,9 @@ async fn main(spawner: Spawner) {
     let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
     spawner.spawn(cyw43_task(runner)).unwrap();
 
-
     let mut display_config = SpiConfig::default();
     display_config.frequency = 16_000_000; // 16 MHz
-    
+
     let spi = Spi::new_blocking(
         p.SPI0,
         p.PIN_18, // SCK
@@ -118,17 +109,14 @@ async fn main(spawner: Spawner) {
     let display_spi = SpiDeviceWithConfig::new(&spi_bus, cs, display_config);
 
     // create display interface with buffer
-    let buffer = [0u8; 512];
     let di = SPIInterface::new(display_spi, dc);
-
     let mut display = Builder::new(ST7735s, di)
         .reset_pin(rst)
         .init(&mut embassy_time::Delay)
         .unwrap();
-    
+
     // test it out
     display.clear(Rgb565::RED).unwrap();
-
     control.init(clm).await;
     control
         .set_power_management(cyw43::PowerManagementMode::PowerSave)
@@ -137,15 +125,13 @@ async fn main(spawner: Spawner) {
     loop {
         if w.is_high() {
             info!("led on!");
-            control.gpio_set(0, true).await;  
-            display.clear(Rgb565::RED).unwrap(); 
-        }
-        else {
+            control.gpio_set(0, true).await;
+            display.clear(Rgb565::RED).unwrap();
+        } else {
             info!("led off!");
             control.gpio_set(0, false).await;
             display.clear(Rgb565::BLACK).unwrap();
             text("led on!", 10, 10).await;
         }
-
     }
 }
